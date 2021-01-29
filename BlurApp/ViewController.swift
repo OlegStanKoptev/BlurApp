9ec5d4286336bed90ac5c1b46df5436cb9d42dcd
Oleg Koptev <olegstankoptev@gmail.com>
2021-01-30 00:08:28 +0300

    LMS auth form recreated

--- BlurApp/ViewController.swift
+++ BlurApp/ViewController.swift
@@ -8,10 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    var blurEffectView: UIVisualEffectView?
+    
+    @IBAction func signInPressed(sender: AnyObject) {
+        print("You pressed sign in")
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+    }
+    
+    func addUnderline(textField: UITextField!) {
+        let border = CALayer();
+        let borderWidth: CGFloat = 1;
+        border.borderColor = UIColor.lightGray.cgColor;
+        border.frame = CGRect(x: 0, y: textField.frame.size.height - borderWidth, width: textField.frame.size.width, height: textField.frame.size.height)
+        border.borderWidth = borderWidth;
+        textField.layer.addSublayer(border)
+        textField.layer.masksToBounds = true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let selectedImageIndex = Int(arc4random_uniform(3))
+        backgroundImageView.image = UIImage(named: "Image\(selectedImageIndex + 1)")
+        backgroundImageView.frame = UIScreen.main.bounds
+        
+        let blurEffect = UIBlurEffect(style: .light)
+        blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView?.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView!)
+               
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        emailTextField.keyboardType = .emailAddress
+        
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        passwordTextField.keyboardType = .asciiCapable
+        passwordTextField.isSecureTextEntry = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        addUnderline(textField: emailTextField)
+        addUnderline(textField: passwordTextField)
     }
 
 
