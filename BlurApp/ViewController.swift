4d2176885e84e347a87db04e8490ab5db351eecb
Oleg Koptev <olegstankoptev@gmail.com>
2021-01-29 22:50:08 +0300

    Initial Commit

---
+++ BlurApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlurApp
+//
+//  Created by Oleg Koptev on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
